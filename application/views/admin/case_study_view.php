423cb601e697efb32d21fbcd88134485f512c1dd
John Brown <jfbrown@pcsenegal-web.local>
2011-09-22 13:49:21 +0000

    fifth commit

--- application/views/admin/case_study_view.php
+++
@@ -1,4 +0,0 @@
-	<h1><?=$title?></h1>
-	<p><u><?php echo $id.'</u>&nbsp;'.anchor('admin/case_study/delete/'.$id, 'Delete').'&nbsp;'.anchor('admin/case_study/update/'.$id, 'Update'); ?><br />
-	<?=$description?></p>
-	<?=$content?>
