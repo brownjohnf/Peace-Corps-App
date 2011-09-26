76dbffbb1cffb8ef6ff8032eedbc3be861972341
John Brown <jack@brownjohnf.com>
2011-09-26 11:52:29 +0000

    updated

--- application/controllers/module.php
+++ application/controllers/module.php
@@ -12,7 +12,6 @@ class Module extends MY_Controller {
 	
 	public function index()
 	{
-		$data = $this->module_class->show();
 		$data['title'] = 'Modules';
 		$data['backtrack'] = array('resource' => 'Resources', 'module' => 'Modules');
 		
