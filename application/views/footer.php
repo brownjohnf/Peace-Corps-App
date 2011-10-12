b0b004c24805ecf5c717c3174d00ddf1fc56ff2e
John Brown <jack@brownjohnf.com>
2011-10-12 11:36:39 +0000

    fixed legal footer width and header spacing on splash page

--- application/views/footer.php
+++ application/views/footer.php
@@ -8,7 +8,8 @@
 			<div id="footer_outer">
 				<div id="footer_upper">
 					<div id="footer_upper_inner">
-						<img src="<?php echo base_url(); ?>img/baobab.png" id="baobab" />
+						<img id="baskets" src="<?php echo base_url(); ?>img/baskets.png" />
+						<img id="baobab" src="<?php echo base_url(); ?>img/baobab.png" />
 						<img id="watermark" src="<?php echo base_url(); ?>img/pc_logo_watermark.png" />
 						
 						<div id="google_search"><!-- Google search -->
