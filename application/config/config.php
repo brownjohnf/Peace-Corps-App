65bec5865f9d17177b7e40d8c73b30d0a0b068a7
John Brown <jack@brownjohnf.com>
2011-11-07 08:29:57 +0000

    pushed to hosting, waiting for DNS to change

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://pcsenegal.com/';
+$config['base_url']	= 'http://pcsenegal.org/';
 
 /*
 |--------------------------------------------------------------------------
--- js/config.js
+++ js/config.js
@@ -4,7 +4,10 @@
 */
 
 
-var base_url = 'http://pcsenegal.com/';
+//var base_url = 'http://pcsenegal.com/';
+var base_url = 'http://alpha.pcsenegal.org/';
 
-//var base_url = 'http://alpha.pcsenegal.org/';
 var upload_dir = base_url + 'uploads/';
+var doc_dir = base_url + 'uploads/docs/';
+var img_dir = base_url + 'uploads/';
+var csv_dir = base_url + 'uploads/csv/';
